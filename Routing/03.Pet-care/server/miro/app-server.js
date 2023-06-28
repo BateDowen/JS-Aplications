eaac96d7a371c6b5ff5885cbaf5f843fbdd7427f
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-06-28 10:46:16 +0300

    giving access-control

--- Routing/03.Pet-care/server/miro/app-server.js
+++ Routing/03.Pet-care/server/miro/app-server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const routes = require('../miro/routes/router')
 const app = express();
+const mongoose = require('mongoose');
 
-app.use(express.json())
+app.use(express.json());
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+    next()
+});
+
 app.use(routes);
 
 const port = 3030;
-
-app.listen(port, console.log(`App is listening on ${port}`));
+mongoose.connect('mongodb://127.0.0.1/pets')
+    .then(
+        app.listen(port, console.log(`App is listening on ${port}`))
+    )
+    .catch(err =>{
+        console.log(err);
+    })
