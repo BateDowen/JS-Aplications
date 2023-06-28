c13b8e3e3aa4bffd94d624c1bce70eb21aab6908
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-06-28 12:33:40 +0300

    creating create post controller

--- Routing/03.Pet-care/server/miro/routes/controllers/dash.js
+++ Routing/03.Pet-care/server/miro/routes/controllers/dash.js
@@ -1,3 +1,30 @@
+const Pet = require('../../modules/Pet');
+
+
 exports.getDashboard = (req,res,next) => {
     console.log(req);
+};
+
+exports.createPost = (req,res,next) => {
+    console.log(req.body);
+    let {name,breed,age,weight,image} = req.body;
+    const pet = new Pet({
+        name,
+        breed,
+        age,
+        weight,
+        image
+    });
+    pet.save()
+    .then(result =>{
+        res.status(201).json({message: 'Pet created!'})
+    })
+    .catch(err =>{
+        if (!err.statusCode) {
+            err.statusCode = 500;
+
+        };
+        next(err);
+        
+    })
 }
--- Routing/03.Pet-care/server/miro/routes/router.js
+++ Routing/03.Pet-care/server/miro/routes/router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const dashController = require('./controllers/dash');
 
-router.get('/data/pets', dashController.getDashboard)
+router.get('/data/pets', dashController.getDashboard);
+
+router.post('/data/pets', dashController.createPost);
+
+
 module.exports = router
